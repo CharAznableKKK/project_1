7f76b282f054e85b28d8bb7fe7323aac9533a479
zhen16 <2587347304@qq.com>
2022-06-01 19:05:18 +0800

    6.1change

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,22 @@
 </template>
 
 <style lang="less">
+body{
+    // 让背景图适应整个屏幕（填满） 哈哈牛逼
+    background-image: url("~@/assets/bg.jpg");
+    background-size: 100% 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;  /*关键*/
+    background-position: center;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    //min-width: 1536px;
+    z-index: -10;
+    zoom:1;
+}
 *{
   margin:0px;
   padding:0px;
--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-      我是页面
-  </div>
+        <header>
+            <h1>实时检测与调控平台</h1>
+        </header>
+    </div>
 </template>
 
 <script>
@@ -9,6 +11,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+header {
+    height: 75px;
+    width: 100%;
+    background-image: url(~@/assets/head_bg.png);
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+}
+
+h1 {
+    font-size: 32px;
+    color: #fff;
+    text-align: center;
+    line-height: 70px;
+}
 
 </style>
